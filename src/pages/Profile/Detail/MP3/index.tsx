4d8c3cfc7e2fc7c0cb6ccb2e895f7e4761339c6f
poet <poet@wanplus.com>
2020-07-09 23:54:55 +0800

    commit: 增加对带鱼屏的兼容，调整MP3的部分排版。

--- src/pages/Profile/Detail/MP3/index.tsx
+++ src/pages/Profile/Detail/MP3/index.tsx
@@ -11,10 +11,10 @@ class ProfileMP3 extends Component<{}, {}>{
     render() {
         return (
             <div className="profile_detail_wrapper">
+                <img className="half" src={Section4} alt="加载失败"/>
                 <img src={Section1} alt="加载失败"/>
                 <img src={Section2} alt="加载失败"/>
                 <img src={Section3} alt="加载失败"/>
-                <img src={Section4} alt="加载失败"/>
                 <video autoPlay muted loop>
                     <source src={`${cdnUrl}/mp3-video1.mp4`}/>
                 </video>
